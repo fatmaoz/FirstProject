39ffa200c5c450363213d03eab13a47ea295da55
fatmaoz <fatmas199021@gmail.com>
2020-10-23 03:04:58 +0100

    Validation

--- src/main/java/com/createproject/selfstudy/api/UserInfoDTO.java
+++ src/main/java/com/createproject/selfstudy/api/UserInfoDTO.java
@@ -2,7 +2,10 @@ package com.createproject.selfstudy.api;
 
 import lombok.*;
 
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -12,7 +15,15 @@ import javax.validation.constraints.NotBlank;
 public class UserInfoDTO {
 
     @NotBlank(message = "Username can not be blank")
+    @Size(min = 3, max = 25, message = "*your name should have at least 3 character")
     private String userName;
+
+    @NotBlank(message = "Crush name can not be blank")
+    @Size(min = 3, max = 25, message = "*crush name should have at least 3 character")
     private String crushName;
+
+    @AssertTrue(message = "you have to aggree to use our app")
+    private boolean termAndCondition;
+
 }
 
